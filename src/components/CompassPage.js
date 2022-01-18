93c49541371afab5e1711fd6e419314692cfcf08
Noam Lubofsky <noamlubofsky@gmail.com>
2022-01-17 23:32:33 -0500

    compass

--- src/components/CompassPage.js
+++ src/components/CompassPage.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import blueleather from '../blueleather.jpeg'
 
 function CompassPage() {
   const [latitude, setLatitude] = useState('')
@@ -100,19 +102,56 @@ function CompassPage() {
 
       return (
         <div>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <title>Document</title>
-          <style dangerouslySetInnerHTML={{__html: "\n      body {\n        display: flex;\n        flex-direction: column;\n        height: 100vh;\n      }\n\n      .compass {\n        position: relative;\n        width: 320px;\n        height: 320px;\n        border-radius: 50%;\n        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);\n        margin: auto;\n      }\n\n      .compass > .arrow {\n        position: absolute;\n        width: 0;\n        height: 0;\n        top: -20px;\n        left: 50%;\n        transform: translateX(-50%);\n        border-style: solid;\n        border-width: 30px 20px 0 20px;\n        border-color: red transparent transparent transparent;\n        z-index: 1;\n      }\n\n      .compass > .compass-circle,\n      .compass > .my-point {\n        position: absolute;\n        width: 90%;\n        height: 90%;\n        top: 50%;\n        left: 50%;\n        transform: translate(-50%, -50%);\n        transition: transform 0.1s ease-out;\n        background: url(https://purepng.com/public/uploads/large/purepng.com-compasscompassinstrumentnavigationcardinal-directionspointsdiagram-1701527842316onq7x.png)\n          center no-repeat;\n        background-size: contain;\n      }\n\n      .compass > .my-point {\n        opacity: 0;\n        width: 20%;\n        height: 20%;\n        background: rgb(8, 223, 69);\n        border-radius: 50%;\n        transition: opacity 0.5s ease-out;\n      }\n\n      .start-btn {\n        margin-bottom: auto;\n      }\n    " }} />
-          <div className="compass">
-            <div className="arrow" />
-            <div className="compass-circle" />
-            <div className="my-point" />
-          </div>
-          <button className="start-btn" onClick={startCompass}>Start compass</button>
+          <Container>
+          <div class="compass">
+  <div class="arrow"></div>
+  <div class="compass-circle"></div>
+  <div class="my-point"></div>
+</div>
+          <Button className="start-btn" onClick={startCompass}>Start Compass</Button>
+          </Container>
         </div>
       );
     }
+
+    const Container = styled.div`
+min-height: 60vh;
+`;
+
+const Button = styled.button`
+
+  margin-top: 5vh;
+  margin-bottom: 2vh;
+  margin-left: 5vw;
+  font-family: 'Times New Roman', Georgia, serif;
+  width: 90vw;
+  height: 6vh;
+  /* line-height: 50px; */
+  font-weight: bold;
+  text-decoration: none;
+  text-align: center;
+  align-items: center;
+//   color: rgb(37, 38, 51);
+  letter-spacing: 1px;
+  /* border: 3px solid #2E6268; */
+  transition: all .35s;
+  justify-content: center;
+  font-size: 3vh;
+  color: silver;
+  background-image: url(${blueleather});
+  background-position: center;
+  background-repeat: no - repeat;
+  background-size: cover;
+  -webkit-touch-callout: none; /* iOS Safari */
+-webkit-user-select: none; /* Safari */
+ -khtml-user-select: none; /* Konqueror HTML */
+   -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none; /* Non-prefixed version, currently
+                              supported by Chrome, Edge, Opera and Firefox */
+  &:hover {
+    cursor: pointer;
+  }
+  `;
 
     export default CompassPage
--- src/components/Extras.js
+++ src/components/Extras.js
@@ -129,6 +129,13 @@ const Button = styled.button`
   background-position: center;
   background-repeat: no - repeat;
   background-size: cover;
+  -webkit-touch-callout: none; /* iOS Safari */
+-webkit-user-select: none; /* Safari */
+ -khtml-user-select: none; /* Konqueror HTML */
+   -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none; /* Non-prefixed version, currently
+                              supported by Chrome, Edge, Opera and Firefox */
   &:hover {
     cursor: pointer;
   }
