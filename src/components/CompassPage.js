6f5ad94c55ade698d1ec3b351f0b8047d7100002
Noam Lubofsky <noamlubofsky@gmail.com>
2022-01-17 22:37:58 -0500

    compass for phone

--- src/components/CompassPage.js
+++ src/components/CompassPage.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 
  function CompassPage() {
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const { latitude, longitude } = position.coords;
+      pointDegree = calcDegreeToPoint(latitude, longitude);
+      if (!isIOS) {
+        window.addEventListener("deviceorientationabsolute", handler, true);
+      }
+      if (pointDegree < 0) {
+        pointDegree = pointDegree + 360;
+      }
+    });
+},[])
+
+  const compassCircle = document.querySelector(".compass-circle");
+  const myPoint = document.querySelector(".my-point");
+  const startBtn = document.querySelector(".start-btn");
+  const isIOS =
+    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+    navigator.userAgent.match(/AppleWebKit/);
+    let compass;
+
+
+  // function init() {
+  //   startBtn.addEventListener("click", startCompass);
+  //   navigator.geolocation.getCurrentPosition(locationHandler);
+
+  //   if (!isIOS) {
+  //     window.addEventListener("deviceorientationabsolute", handler, true);
+  //   }
+  // }
+
+  function startCompass() {
+    if (isIOS) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === "granted") {
+            window.addEventListener("deviceorientation", handler, true);
+          } else {
+            alert("has to be allowed!");
+          }
+        })
+        .catch(() => alert("not supported"));
+    }
+  }
+
+  function handler(e) {
+    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+    compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+
+    // ±15 degree
+    if (
+      (pointDegree < Math.abs(compass) &&
+        pointDegree + 15 > Math.abs(compass)) ||
+      pointDegree > Math.abs(compass + 15) ||
+      pointDegree < Math.abs(compass)
+    ) {
+      myPoint.style.opacity = 0;
+    } else if (pointDegree) {
+      myPoint.style.opacity = 1;
+    }
+  }
+
+  let pointDegree;
+
+  // function locationHandler(position) {
+  //   const { latitude, longitude } = position.coords;
+  //   pointDegree = calcDegreeToPoint(latitude, longitude);
+
+  //   if (pointDegree < 0) {
+  //     pointDegree = pointDegree + 360;
+  //   }
+  // }
+
+  function calcDegreeToPoint(latitude, longitude) {
+    // Qibla geolocation
+    const point = {
+      lat: 21.422487,
+      lng: 39.826206
+    };
+
+    const phiK = (point.lat * Math.PI) / 180.0;
+    const lambdaK = (point.lng * Math.PI) / 180.0;
+    const phi = (latitude * Math.PI) / 180.0;
+    const lambda = (longitude * Math.PI) / 180.0;
+    const psi =
+      (180.0 / Math.PI) *
+      Math.atan2(
+        Math.sin(lambdaK - lambda),
+        Math.cos(phi) * Math.tan(phiK) -
+          Math.sin(phi) * Math.cos(lambdaK - lambda)
+      );
+    return Math.round(psi);
+  }
+
       return (
         <div>
           <meta charSet="UTF-8" />
@@ -14,7 +109,7 @@ import React from "react";
             <div className="compass-circle" />
             <div className="my-point" />
           </div>
-          <button className="start-btn">Start compass</button>
+          <button className="start-btn" onClick={startCompass}>Start compass</button>
         </div>
       );
     }
